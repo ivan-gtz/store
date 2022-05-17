065122d1e71335bb9e888243d8c2db4ed0b6df9f
ivan-gtz <ivangutierz20@gmail.com>
2022-05-17 09:09:34 -0400

    Bug

--- pages/index.tsx
+++ pages/index.tsx
@@ -82,7 +82,7 @@ export const HomeRoute:FC<Props> = ({ products }) => {
             as={Link} 
             width='fit-content'
             colorScheme='whatsapp' 
-            href={`https://wa.me/591%2072472714?text=${encodeURIComponent(text)}`} 
+            href={`https://wa.me/+591-(004)72472714?text=${encodeURIComponent(text)}`} 
             isExternal
           >
             Completar pedido ({cart.length} productos)
